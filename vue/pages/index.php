27b1b2f928e763573d55d8e454b5b742f3f8a6dc
Kyusaor <kyu.chrysaor@gmail.com>
2025-06-01 17:05:10 +0200

    creation pages de base + style

---
+++ vue/pages/index.php
@@ -0,0 +1,24 @@
+<?php
+include '../../src/controler/session.php';
+$translations = $_SESSION['translations'];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include '../../vue/partials/head.html'; ?>
+    <title><?= getTitle() ?></title>
+</head>
+
+<body>
+
+<?php
+include '../../vue/partials/header.php';
+include '../../vue/partials/navmenu.php';
+?>
+
+<main>
+    <p><?= $translations->get('global.navbar.about') ?></p>
+</main>
+</body>
+</html>
